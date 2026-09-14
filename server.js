7eb715799ed48b172f24c2782b1bed73c005778d
japiw <vier2706@gmail.com>
2026-09-14 11:11:20 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+
+  res.end(
+    JSON.stringify({
+      message: "Hello from DevOps Lab!",
+      status: "running",
+    })
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
